a6a628d8ccb867553ca0542cae4d1b8e160c9c93
skendis <sergei.kendis@gmail.com>
2021-05-11 16:51:15 +0300

    add server and app

---
+++ apps/fnbc-server/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'fnbc-server',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/fnbc-server',
+};
---
+++ apps/fnbc-server/src/main.ts
@@ -0,0 +1,23 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import * as express from 'express';
+import * as path from 'path';
+
+const staticPath = `${__dirname}/../fnbc`;
+const app = express();
+
+app.use(express.static(staticPath));
+
+app.get('/*', function(req, res) {
+
+  res.sendFile(path.join(`${staticPath}/index.html`));
+});
+
+const port = process.env.port || 8080;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
+server.on('error', console.error);
