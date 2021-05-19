91d93647fed4632cea14a4f4a4aeeeb28565dd97
skendis <sergei.kendis@gmail.com>
2021-05-20 00:55:04 +0300

    add login and register page

--- apps/fnbc/src/app/app.module.ts
+++ apps/fnbc/src/app/app.module.ts
@@ -2,17 +2,46 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'incomes',
+    loadChildren: () => import('./incomes/incomes.module').then(m => m.IncomesModule)
+  },
+  {
+    path: 'goals',
+    loadChildren: () => import('./goals/goals.module').then(m => m.GoalsModule)
+  },
+  {
+    path: 'onboarding',
+    loadChildren: () => import('./onboarding/onboarding.module').then(m => m.OnboardingModule)
+  },
+  {
+    path: '', redirectTo: '/auth', pathMatch: 'full'
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  }
+];
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    RouterModule.forRoot(routes, { initialNavigation: 'enabled' }),
     NgbModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
