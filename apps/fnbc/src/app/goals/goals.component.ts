5d3090b04166a0d462ba9e3e6ccd67c6d862ae5b
skendis <sergei.kendis@gmail.com>
2021-05-20 01:00:37 +0300

    add login and register page

---
+++ apps/fnbc/src/app/goals/goals.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'financial-independence-goals',
+  templateUrl: './goals.component.html',
+  styleUrls: ['./goals.component.scss']
+})
+export class GoalsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
